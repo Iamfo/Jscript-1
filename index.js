7d1b35f9e4574eb6164fbca2fe07ee7e68077e98
Francis Ojiako <focathlon@gmail.com>
2025-04-28 21:16:52 +0200

    esercitazione

---
+++ index.js
@@ -0,0 +1,15 @@
+const product = {
+    id: 1,
+    title: "pcPortatile",
+    price: 1200
+}
+
+console.log(product);
+
+product.id = 3;
+
+cont key = "price";
+
+product[key] =1400;
+
+console.log(product); 
