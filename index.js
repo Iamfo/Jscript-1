eb732ededa0925cc9dbab66bea2bf04a6db3d637
Francis Ojiako <focathlon@gmail.com>
2025-05-18 19:08:15 +0200

    jscript-5

--- index.js
+++ index.js
@@ -1,15 +1,91 @@
-const product = {
-    id: 1,
-    title: "pcPortatile",
-    price: 1200
-}
+/**
+ * Esercitazione:
 
-console.log(product);
+ * Dobbiamo creare una pagina dove abbiamo un paragrafo, un input email, un input password e un bottone. L'utente dovrà inserire i valori di email e password e al click il sistema deve controllare che i valori siano corretti.
 
-product.id = 3;
+ * Se email le password corrispondono allora all'interno del paragrafo appare la scritta "Benvenuto, Utente" altrimenti la scritta "Credenziali errate".
 
-cont key = "price";
+ * Le credenziali corrette sono:
+ * const state = {
+    // Altre chavi...
+    auth: {
+      email: "user@example.com",
+      password: "1234",
+    }
+ * }
 
-product[key] =1400;
+ * Se email o password non vengono inserite, al click del bottone nel paragrafo appare la scritta "Tutti i campi sono obbligatori"; 
+ * 
+ */
 
-console.log(product); 
+
+// 1. Selezione degli elementi HTML
+
+const $emailInput = document.querySelector("#email");
+const $passwordInput = document.querySelector("#password");
+const $loginBtn = document.querySelector("#login");
+const $messageP = document.querySelector("#message");
+
+// 2. lo state con le Chiave
+
+const state = {
+    userInput: {
+        email: "",
+        password: "",
+    },
+    auth: {
+        email: "user@example.com",
+        password: "1234",
+    },
+    message: ""
+};
+
+// 3. Funzioni
+
+const authenticate = () => {
+    const { email, password } = state.userInput;
+    
+// 4. Verifica delle credenziali
+
+    if (email === state.auth.email && password === state.auth.password) {
+        state.message = "Benvenuto, Utente";
+    } else {
+        state.message = "Credenziali errate";
+    }
+
+    if (!email || !password) {
+        state.message = "Tutti i campi sono obbligatori";
+        return;
+    }
+
+};
+
+// 5. Funzione di rendering
+const render = () => {
+    $messageP.innerHTML = state.message;
+};
+
+// 6. Event listeners
+const manageListeners = () => {
+    $emailInput.addEventListener("input", (event) => {
+        state.userInput.email = event.target.value();
+    });
+
+    $passwordInput.addEventListener("input", (event) => {
+        state.userInput.password = event.target.value;
+    });
+
+    $loginBtn.addEventListener("click", () => {
+        authenticate();
+        render();
+    });
+};
+
+// 7. Mount
+const init = () => {
+    render();
+    manageListeners();
+};
+
+// 8. Inizializzazione
+init();
